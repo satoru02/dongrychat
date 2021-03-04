6866496bfc11eeb1ad5616688a52b7dd46cec4a6
satoru02 <sugiwaka02@gmail.com>
2021-03-04 17:44:38 +0900

    [setting/#3_exclude_files_setting]
    🛤 Setting exlude files:

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,10 @@ module LivingRoom
     # the framework and any gems in your application.
 
     # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework false
+      g.helper false
+      g.assets false
+    end
   end
 end
