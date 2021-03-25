a3c63bfa15d5e2919c6d38f7c5fbb9623cfbe98c
satoru02 <sugiwaka02@gmail.com>
2021-03-25 16:28:45 +0900

    #67 add production setting to jwt_session.rb

--- config/initializers/jwt_session.rb
+++ config/initializers/jwt_session.rb
@@ -7,4 +7,11 @@ if Rails.env["development"]
     redis_db_name: "0",
     token_prefix: "jwt_"
   }
+else Rails.env["production"]
+  JWTSessions.token_store = :redis, {
+    redis_host: "127.0.0.1",
+    redis_port: "6379",
+    redis_db_name: "0",
+    token_prefix: "jwt_"
+    }
 end
