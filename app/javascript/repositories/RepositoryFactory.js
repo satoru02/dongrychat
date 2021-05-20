21057f39d004d91b9c959143c89af765814cc3ee
satoru02 <sugiwaka02@gmail.com>
2021-05-20 17:49:23 +0900

    #191 add tags repository_factory

--- app/javascript/repositories/RepositoryFactory.js
+++ app/javascript/repositories/RepositoryFactory.js
@@ -1,11 +1,13 @@
 import spacesRepository from './spacesRepository';
 import usersRepository from './usersRepository';
 import relationshipsRepository from './relationshipsRepository';
+import tagsRepository from './tagsRepository';
 
 const repositories = {
   spaces: spacesRepository,
   users: usersRepository,
-  relationships: relationshipsRepository
+  relationships: relationshipsRepository,
+  tags: tagsRepository
 };
 
 export const RepositoryFactory = {
