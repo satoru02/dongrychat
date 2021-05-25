5ce2bbbe8cd41c6e4f80add694427042f81e7dc0
satoru02 <sugiwaka02@gmail.com>
2021-05-25 11:06:36 +0900

    #185 fix webpack setting

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,7 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-environment.plugins.prepend('BundleAnalyzerPlugin', new BundleAnalyzerPlugin())
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 module.exports = environment
